d72e350272f39dfc5e70d00d5c0bc6da1fa1a923
Felix <jonfelixrico@gmail.com>
2021-07-31 23:41:50 +0800

    Prevent from opening a browser automatically on server start

--- quasar.conf.js
+++ quasar.conf.js
@@ -83,7 +83,7 @@ module.exports = configure(function (ctx) {
     devServer: {
       https: false,
       port: 8080,
-      open: true, // opens browser window automatically,
+      open: false, // opens browser window automatically,
       onBeforeSetupMiddleware: require('./mocks')
     },
 
