401a930ac55ce2d18be50635f3fe05040ae1a643
Felix <jonfelixrico@gmail.com>
2021-08-01 14:49:28 +0800

    Add a few test code to SelectedTurtle

--- src/pages/SelectedTurtle.vue
+++ src/pages/SelectedTurtle.vue
@@ -1,17 +1,30 @@
 <template>
   <div>
-    {{ turtleId }}
+    <q-separator />
+    <div class="q-pa-xs">
+      <div>{{ turtle.id }} / {{ turtle.name }}</div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { useStore } from 'src/store'
+import { defineComponent, computed } from 'vue'
 
 export default defineComponent({
   props: {
     turtleId: {
       type: String,
       required: true
+    }
+  },
+
+  setup (props) {
+    const store = useStore()
+    const turtle = computed(() => store.state.turtles.turtles[props.turtleId])
+
+    return {
+      turtle
     }
   }
 })
