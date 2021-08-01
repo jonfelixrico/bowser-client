bf478932f2c69b90995f1ffd5f182616db63c60b
Felix <jonfelixrico@gmail.com>
2021-08-02 02:53:33 +0800

    Display next coordinates when a control is pressed

--- src/components/CAbsoluteControls.vue
+++ src/components/CAbsoluteControls.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="q-gutter-y-sm">
     <div class="row justify-center">
+      {{ nextCoords.north }}
+    </div>
+    <div class="row justify-center">
       <q-btn round unelevated color="primary" icon="la la-arrow-up" @click="onZMove(-1)" />
     </div>
-    <div class="row justify-center q-gutter-x-sm">
+    <div class="row justify-center q-gutter-x-sm items-center">
+        {{ nextCoords.west }}
         <q-btn round unelevated color="primary" icon="la la-arrow-left"  @click="onXMove(-1)" />
         <q-btn round color="primary" icon="la la-arrow-down" @click="onZMove(1)" />
         <q-btn round unelevated color="primary" icon="la la-arrow-right" @click="onXMove(1)" />
+        {{ nextCoords.east }}
+    </div>
+    <div class="row justify-center">
+      {{ nextCoords.south }}
     </div>
   </div>
 </template>
@@ -24,6 +32,13 @@ export interface ISteps {
 export interface IHorizontalCoords {
   x: number
   z: number
+}
+
+interface INextCoords {
+  north: IHorizontalCoords,
+  south: IHorizontalCoords,
+  east: IHorizontalCoords,
+  west: IHorizontalCoords
 }
 
 // TODO use the component api
@@ -45,25 +60,28 @@ export default defineComponent({
   },
 
   computed: {
-    x: {
-      get (): number {
-        return this.coords.x
+    coordsModel: {
+      get (): IHorizontalCoords {
+        return this.coords
       },
 
-      set (x: number) {
-        this.$emit('update:coords', { ...this.coords, x })
+      set (coords: IHorizontalCoords) {
+        this.$emit('update:coords', coords)
       }
     },
 
-    z: {
-      get (): number {
-        return this.coords.z
-      },
+    nextCoords (): INextCoords {
+      const { coords, steps } = this
+      const { north, south, east, west } = steps
+      const { x, z } = coords
 
-      set (z: number) {
-        this.$emit('update:coords', { ...this.coords, z })
+      return {
+        north: { x, z: z - north },
+        south: { x, z: z + south },
+        east: { z, x: x + east },
+        west: { z, x: x - west }
       }
-    },
+    }
   },
 
   methods: {
@@ -72,13 +90,8 @@ export default defineComponent({
      * @param direction 1 for east, -1 for west. Only these two coordss are expected.
      */
     onXMove (direction: number) {
-      const { east, west } = this.steps
-
-      if (direction > 1) {
-        this.x += east
-      } else { // only coords here would be -1
-        this.x -= west
-      }
+      const { east, west } = this.nextCoords
+      this.coordsModel = direction > 0 ? east : west
     },
 
     /**
@@ -86,13 +99,8 @@ export default defineComponent({
      * @param direction 1 for south, -1 for north. Only these two coordss are expected.
      */
     onZMove (direction: number) {
-      const { north, south } = this.steps
-
-      if (direction > 1) {
-        this.z += south
-      } else { // only coords here would be -1
-        this.z -= north
-      }
+      const { south, north } = this.nextCoords
+      this.coordsModel = direction > 0 ? south : north
     },
   }
 })
