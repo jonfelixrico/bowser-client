85f824a7ab934fbb701d4268166cc6b304e40c79
Felix <jonfelixrico@gmail.com>
2021-08-01 22:01:10 +0800

    Move server-associated operations in MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -19,10 +19,35 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, watch } from 'vue'
+import { useEventSource } from '@vueuse/core'
 import CTurtleOverviewDrawer from '../components/CTurtleOverviewDrawer.vue'
+import { api } from 'src/boot/axios'
+import { getStore } from 'src/store'
 
 export default defineComponent({
   components: { CTurtleOverviewDrawer },
+
+  setup () {
+    const { status, data } = useEventSource(`${api.defaults.baseURL || ''}/sse`)
+    watch(status, (value) => {
+      if (value === 'OPEN') {
+        watch(data, (data) => {
+          console.debug(data)
+        })
+      }
+    })
+  },
+
+  async beforeRouteEnter (to, from, next) {
+    const store = getStore()
+
+    if (!store) {
+      return next(false)
+    }
+
+    await store.dispatch('turtles/fetchTurtleList')
+    next()
+  },
 })
 </script>
