10247407aedfed3b44491fa5e13770a26fcce374
Felix <jonfelixrico@gmail.com>
2021-08-01 18:05:15 +0800

    Change the env key used for setting Axios' `baseURL`

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -15,7 +15,7 @@ declare module '@vue/runtime-core' {
 // for each client)
 
 // TODO set to proper API via env
-const api = axios.create({ baseURL: process.env.API })
+const api = axios.create({ baseURL: process.env.API_BASE_URL })
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
