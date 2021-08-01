665b0e572d0c3409173a60a2dd81193fe5c676b3
Felix <jonfelixrico@gmail.com>
2021-08-01 21:34:22 +0800

    Remove the SSE boot file

--- quasar.conf.js
+++ quasar.conf.js
@@ -59,11 +59,7 @@ module.exports = configure(function (ctx) {
     // https://v2.quasar.dev/quasar-cli/boot-files
     boot: [
       'i18n',
-      'axios',
-
-      // custom boot files
-
-      'sse'
+      'axios'
     ],
 
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
