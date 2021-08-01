ea6bee9203ea02c29e521deb9537b82c7fa657f5
Felix <jonfelixrico@gmail.com>
2021-08-01 14:20:14 +0800

    Add slot in CLayerDrawer

--- src/components/CLayerDrawer.vue
+++ src/components/CLayerDrawer.vue
@@ -1,20 +1,26 @@
 <template>
   <q-drawer v-bind="$attrs">
-    <q-scroll-area class="fit">
-      <q-list separator>
-        <q-item v-for="{ x, z, id, label } of turtles" :key="id" clickable>
-          <q-item-section>
-            <!-- TODO i18nize these -->
-            <q-item-label>{{ label ? label : 'No Label' }}</q-item-label>
-            <q-item-label caption>Turtle id {{ id }}</q-item-label>
-          </q-item-section>
+    <div class="fit column">
+      <div class="col relative-position">
+        <q-scroll-area class="absolute-full">
+          <q-list separator>
+            <q-item v-for="{ x, z, id, label } of turtles" :key="id" clickable>
+              <q-item-section>
+                <!-- TODO i18nize these -->
+                <q-item-label>{{ label ? label : 'No Label' }}</q-item-label>
+                <q-item-label caption>Turtle id {{ id }}</q-item-label>
+              </q-item-section>
 
-          <q-item-section side>
-            <q-item-label>{{ `${x}, ${z}` }}</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-scroll-area>
+              <q-item-section side>
+                <q-item-label>{{ `${x}, ${z}` }}</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+      </div>
+
+      <slot />
+    </div>
   </q-drawer>
 </template>
 
