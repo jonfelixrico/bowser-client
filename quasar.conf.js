7cf268ed4e563ed47ca57010c3f137f97e365505
Felix <jonfelixrico@gmail.com>
2021-08-01 18:19:00 +0800

    Use history mode for VueRouter

--- quasar.conf.js
+++ quasar.conf.js
@@ -83,7 +83,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       env: envConfig,
 
       // transpile: false,
