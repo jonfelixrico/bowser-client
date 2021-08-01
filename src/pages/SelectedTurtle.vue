ea56a4e1d55ccf4f8ab8b169160d08f1ec057e07
Felix <jonfelixrico@gmail.com>
2021-08-01 17:39:29 +0800

    Implement sending of commands in SelectedTurtle

--- src/pages/SelectedTurtle.vue
+++ src/pages/SelectedTurtle.vue
@@ -10,17 +10,17 @@
     <div class="q-pa-sm">
       <div class="q-gutter-y-sm">
       <div class="row justify-center">
-        <q-btn round unelevated color="primary" icon="la la-arrow-up"  />
+        <q-btn round unelevated color="primary" icon="la la-arrow-up" @click="onZMove(-1)" />
       </div>
       <div class="row">
         <q-space />
-        <q-btn round unelevated color="primary" icon="la la-arrow-left"  />
+        <q-btn round unelevated color="primary" icon="la la-arrow-left"  @click="onXMove(-1)" />
         <q-space />
-        <q-btn round unelevated color="primary" icon="la la-arrow-right"  />
+        <q-btn round unelevated color="primary" icon="la la-arrow-right" @click="onXMove(1)" />
         <q-space />
       </div>
       <div class="row justify-center">
-        <q-btn round color="primary" icon="la la-arrow-down"  />
+        <q-btn round color="primary" icon="la la-arrow-down" @click="onZMove(1)" />
       </div>
     </div>
     </div>
@@ -28,8 +28,14 @@
 </template>
 
 <script lang="ts">
+import { api } from 'src/boot/axios'
 import { useStore } from 'src/store'
 import { defineComponent, computed } from 'vue'
+
+interface IAction {
+  action: string,
+  args: (string | number)[]
+}
 
 export default defineComponent({
   props: {
@@ -43,8 +49,34 @@ export default defineComponent({
     const store = useStore()
     const turtle = computed(() => store.state.turtles.turtles[props.turtleId])
 
+    async function sendCommand (action: IAction) {
+      await api.post('commands', {
+        commands: {
+          id: props.turtleId,
+          actions: [action]
+        }
+      })
+    }
+
+    async function onXMove (offset: number) {
+      await sendCommand({
+        action: 'MOVE_TO_X',
+        args: [turtle.value.x + offset]
+      })
+    }
+
+    async function onZMove (offset: number) {
+      await sendCommand({
+        action: 'MOVE_TO_Z',
+        args: [turtle.value.z + offset]
+      })
+    }
+
     return {
-      turtle
+      turtle,
+
+      onXMove,
+      onZMove
     }
   }
 })
