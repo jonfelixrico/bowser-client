6518f86337be8537213b40206cb85c0ff655a71c
Felix <jonfelixrico@gmail.com>
2021-08-01 12:57:39 +0800

    Create layout component for the visualizer

---
+++ src/layouts/VisualizerLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-layout view="lHh Lpr lFf" container :style="{ height: `${height}px` }">
+    <q-drawer model-value>
+      test
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    height: Number
+  }
+})
+</script>
