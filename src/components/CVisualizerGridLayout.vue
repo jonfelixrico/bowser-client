f98fd0d55d6b3e929ce2f8297878fe9f82d12eab
Felix <jonfelixrico@gmail.com>
2021-08-03 00:40:13 +0800

    Create a slot-only version of CVisualizerContent

---
+++ src/components/CVisualizerGridLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page :style-fn="styleFn">
+    <q-scroll-area :style="{ height: `${pageHeight}px` }" class="full-width">
+      <div class="flex flex-center q-pa-sm" :style="{ minHeight: `${pageHeight}px` }">
+        <slot />
+      </div>
+    </q-scroll-area>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useQPageStyleFn } from 'src/composition/useQPageStyleFn'
+
+export default defineComponent({
+  setup () {
+    return {
+      ...useQPageStyleFn(),
+    }
+  },
+})
+</script>
