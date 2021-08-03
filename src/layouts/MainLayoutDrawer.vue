f384c3dce3de2b971ea9ea88560618aee57fe1c6
Felix <jonfelixrico@gmail.com>
2021-08-03 22:38:24 +0800

    Integrate CLayerTurtlesList into the MainLayoutDrawer

--- src/layouts/MainLayoutDrawer.vue
+++ src/layouts/MainLayoutDrawer.vue
@@ -1,14 +1,29 @@
 <template>
   <q-drawer class="column" v-bind="$attrs">
     <c-layer-overview class="col" />
+    <template v-if="showLayerList">
+      <q-separator />
+      <c-layer-turtles-list class="col" />
+    </template>
   </q-drawer>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, computed } from 'vue'
+import { useRoute } from 'vue-router'
 import CLayerOverview from 'components/CLayerOverview.vue'
+import CLayerTurtlesList from 'src/components/CLayerTurtlesList.vue'
 
 export default defineComponent({
-  components: { CLayerOverview }
+  components: { CLayerOverview, CLayerTurtlesList },
+
+  setup () {
+    const route = useRoute()
+    const showLayerList = computed(() => !!route.params.yLevel)
+
+    return {
+      showLayerList
+    }
+  }
 })
 </script>
