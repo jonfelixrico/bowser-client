9f53c7dea24e48083a2a466be8657e544774817a
Felix <jonfelixrico@gmail.com>
2021-08-04 23:23:27 +0800

    Create getter for the 'flagged as busy' state

--- src/store/turtles/getters.ts
+++ src/store/turtles/getters.ts
@@ -3,9 +3,13 @@ import { StateInterface } from '../index'
 import { ITurtleState } from './state'
 
 const getters: GetterTree<ITurtleState, StateInterface> = {
-  someAction (/* context */) {
-    // your code
-  },
+  flaggedAsBusy ({ flaggedAsBusyMap }): Set<string> {
+    const setValue = Object.entries(flaggedAsBusyMap)
+      .filter(entry => entry[1] > 0)
+      .map(([key]) => key)
+
+    return new Set(setValue)
+  }
 }
 
 export default getters
