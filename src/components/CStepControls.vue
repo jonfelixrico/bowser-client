1962b05c9deb449661a48c4bdaa7a180a0439b4b
Felix <jonfelixrico@gmail.com>
2021-08-04 21:04:43 +0800

    Change CStepControls to CTurtleControls

---
+++ src/components/CStepControls.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="column justify-between q-gutter-y-sm">
+    <div>Steps</div>
+    <q-separator />
+    <div class="row q-gutter-x-sm">
+      <q-input class="col" label="North" outlined dense />
+      <q-input class="col" label="South" outlined dense />
+      <q-checkbox class="col-3">Lock Y</q-checkbox>
+    </div>
+    <div class="row q-gutter-x-sm">
+      <q-input class="col" label="East" outlined dense />
+      <q-input class="col" label="West" outlined dense />
+      <q-checkbox class="col-3">Lock X</q-checkbox>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+})
+</script>
--- src/layouts/MainLayoutFooter.vue
+++ src/layouts/MainLayoutFooter.vue
@@ -8,7 +8,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import CTurtleControls from 'components/CTurtleControls.vue'
+import CTurtleControls from 'src/components/CStepControls.vue'
 
 export default defineComponent({
   components: { CTurtleControls }
