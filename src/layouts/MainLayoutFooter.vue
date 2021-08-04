e129b0e5cfd14774c2b4d08c27d8af35b9c94472
Felix <jonfelixrico@gmail.com>
2021-08-04 21:06:51 +0800

    Remove CStepControls

--- src/layouts/MainLayoutFooter.vue
+++ src/layouts/MainLayoutFooter.vue
@@ -1,16 +1,13 @@
 <template>
   <q-footer class="bg-transparent text-black q-pa-sm">
     <div class="row full-width">
-      <c-turtle-controls />
     </div>
   </q-footer>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import CTurtleControls from 'src/components/CStepControls.vue'
 
 export default defineComponent({
-  components: { CTurtleControls }
 })
 </script>
