d7e20821244659b8c2f1572bba99ee06f57ce73b
Felix <jonfelixrico@gmail.com>
2021-08-04 21:12:49 +0800

    Implement GoTo buttons

---
+++ src/components/CAbsolutePositionControls.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="column q-gutter-y-sm">
+    <q-btn unelevated no-caps color="primary" label="Go to X" icon="la la-arrows-alt-h" />
+    <q-btn unelevated no-caps color="primary" label="Go to Y" icon="la la-layer-group" />
+    <q-btn unelevated no-caps color="primary" label="Go to Z" icon="la la-arrows-alt-v" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+})
+</script>
--- src/layouts/MainLayoutFooter.vue
+++ src/layouts/MainLayoutFooter.vue
@@ -1,13 +1,16 @@
 <template>
   <q-footer class="bg-transparent text-black q-pa-sm">
     <div class="row full-width">
+      <c-absolute-position-controls />
     </div>
   </q-footer>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import CAbsolutePositionControls from 'components/CAbsolutePositionControls.vue'
 
 export default defineComponent({
+  components: { CAbsolutePositionControls },
 })
 </script>
