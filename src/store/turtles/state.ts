f8b125c0f22d4c13f5aaceff3abd1b8f603bdc7d
Felix <jonfelixrico@gmail.com>
2021-08-04 23:14:46 +0800

    Intergate ICommand and ICompletedCommand into the turlte state

--- src/store/turtles/state.ts
+++ src/store/turtles/state.ts
@@ -1,12 +1,18 @@
+import { ICommand } from 'src/models/command.interface'
+import { ICompletedCommand } from 'src/models/completed-command.interface'
 import { ITurtle } from 'src/models/turtle.interface'
 
 export interface ITurtleState {
-  turtles: { [turtleId: string]: ITurtle }
+  turtles: { [turtleId: string]: ITurtle },
+  commandStream: ICommand[],
+  executeStream: ICompletedCommand[]
 }
 
 function state (): ITurtleState {
   return {
     turtles: {},
+    commandStream: [],
+    executeStream: []
   }
 }
 
